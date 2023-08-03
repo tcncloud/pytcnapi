ba0cff4db1187ba0bdf57be1ed2189f9c0926af5
TCN CI <ci@tcnp3.com>
2023-08-03 20:02:21 +0000

    update pytcnapi > fix: formatting

--- api/commons/workflows/nodes_pb2.pyi
+++ api/commons/workflows/nodes_pb2.pyi
@@ -1,6 +1,7 @@
 from api.commons.workflows import example_pb2 as _example_pb2
 from api.commons.workflows import omni_pb2 as _omni_pb2
 from api.commons.workflows import omni_bot_pb2 as _omni_bot_pb2
+from api.commons.workflows import test_bot_pb2 as _test_bot_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
@@ -9,7 +10,7 @@ from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Map
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class NodeDefinition(_message.Message):
-    __slots__ = ["id", "name", "description", "outputs", "error_node_id", "print", "random", "console_input", "comparator", "store_input", "chatbot", "omni_prompt", "omni_set_skill", "omni_to_agent", "omni_error", "omni_bot_test_start", "omni_bot_test_step", "omni_bot_test_end"]
+    __slots__ = ["id", "name", "description", "outputs", "error_node_id", "print", "random", "console_input", "comparator", "store_input", "chatbot", "omni_prompt", "omni_set_skill", "omni_to_agent", "omni_error", "omni_bot_test_start", "omni_bot_test_step", "omni_bot_test_end", "test_bot_test_start", "test_bot_test_step", "test_bot_test_end"]
     ID_FIELD_NUMBER: _ClassVar[int]
     NAME_FIELD_NUMBER: _ClassVar[int]
     DESCRIPTION_FIELD_NUMBER: _ClassVar[int]
@@ -28,6 +29,9 @@ class NodeDefinition(_message.Message):
     OMNI_BOT_TEST_START_FIELD_NUMBER: _ClassVar[int]
     OMNI_BOT_TEST_STEP_FIELD_NUMBER: _ClassVar[int]
     OMNI_BOT_TEST_END_FIELD_NUMBER: _ClassVar[int]
+    TEST_BOT_TEST_START_FIELD_NUMBER: _ClassVar[int]
+    TEST_BOT_TEST_STEP_FIELD_NUMBER: _ClassVar[int]
+    TEST_BOT_TEST_END_FIELD_NUMBER: _ClassVar[int]
     id: str
     name: str
     description: str
@@ -46,4 +50,7 @@ class NodeDefinition(_message.Message):
     omni_bot_test_start: _omni_bot_pb2.OmniBotNodeTestStart
     omni_bot_test_step: _omni_bot_pb2.OmniBotNodeTestStep
     omni_bot_test_end: _omni_bot_pb2.OmniBotNodeTestEnd
-    def __init__(self, id: _Optional[str] = ..., name: _Optional[str] = ..., description: _Optional[str] = ..., outputs: _Optional[_Iterable[str]] = ..., error_node_id: _Optional[str] = ..., print: _Optional[_Union[_example_pb2.NodePrint, _Mapping]] = ..., random: _Optional[_Union[_example_pb2.NodeRandom, _Mapping]] = ..., console_input: _Optional[_Union[_example_pb2.NodeConsoleInput, _Mapping]] = ..., comparator: _Optional[_Union[_example_pb2.NodeComparator, _Mapping]] = ..., store_input: _Optional[_Union[_example_pb2.NodeStoreInput, _Mapping]] = ..., chatbot: _Optional[_Union[_example_pb2.NodeChatbot, _Mapping]] = ..., omni_prompt: _Optional[_Union[_omni_pb2.OmniNodePrompt, _Mapping]] = ..., omni_set_skill: _Optional[_Union[_omni_pb2.OmniNodeSetSkill, _Mapping]] = ..., omni_to_agent: _Optional[_Union[_omni_pb2.OmniNodeToAgent, _Mapping]] = ..., omni_error: _Optional[_Union[_omni_pb2.OmniNodeError, _Mapping]] = ..., omni_bot_test_start: _Optional[_Union[_omni_bot_pb2.OmniBotNodeTestStart, _Mapping]] = ..., omni_bot_test_step: _Optional[_Union[_omni_bot_pb2.OmniBotNodeTestStep, _Mapping]] = ..., omni_bot_test_end: _Optional[_Union[_omni_bot_pb2.OmniBotNodeTestEnd, _Mapping]] = ...) -> None: ...
+    test_bot_test_start: _test_bot_pb2.TestBotNodeTestStart
+    test_bot_test_step: _test_bot_pb2.TestBotNodeTestStep
+    test_bot_test_end: _test_bot_pb2.TestBotNodeTestEnd
+    def __init__(self, id: _Optional[str] = ..., name: _Optional[str] = ..., description: _Optional[str] = ..., outputs: _Optional[_Iterable[str]] = ..., error_node_id: _Optional[str] = ..., print: _Optional[_Union[_example_pb2.NodePrint, _Mapping]] = ..., random: _Optional[_Union[_example_pb2.NodeRandom, _Mapping]] = ..., console_input: _Optional[_Union[_example_pb2.NodeConsoleInput, _Mapping]] = ..., comparator: _Optional[_Union[_example_pb2.NodeComparator, _Mapping]] = ..., store_input: _Optional[_Union[_example_pb2.NodeStoreInput, _Mapping]] = ..., chatbot: _Optional[_Union[_example_pb2.NodeChatbot, _Mapping]] = ..., omni_prompt: _Optional[_Union[_omni_pb2.OmniNodePrompt, _Mapping]] = ..., omni_set_skill: _Optional[_Union[_omni_pb2.OmniNodeSetSkill, _Mapping]] = ..., omni_to_agent: _Optional[_Union[_omni_pb2.OmniNodeToAgent, _Mapping]] = ..., omni_error: _Optional[_Union[_omni_pb2.OmniNodeError, _Mapping]] = ..., omni_bot_test_start: _Optional[_Union[_omni_bot_pb2.OmniBotNodeTestStart, _Mapping]] = ..., omni_bot_test_step: _Optional[_Union[_omni_bot_pb2.OmniBotNodeTestStep, _Mapping]] = ..., omni_bot_test_end: _Optional[_Union[_omni_bot_pb2.OmniBotNodeTestEnd, _Mapping]] = ..., test_bot_test_start: _Optional[_Union[_test_bot_pb2.TestBotNodeTestStart, _Mapping]] = ..., test_bot_test_step: _Optional[_Union[_test_bot_pb2.TestBotNodeTestStep, _Mapping]] = ..., test_bot_test_end: _Optional[_Union[_test_bot_pb2.TestBotNodeTestEnd, _Mapping]] = ...) -> None: ...
