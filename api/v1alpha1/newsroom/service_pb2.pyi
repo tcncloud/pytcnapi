4d6adc98edcca561143cca6ddd70789ce360192d
TCN CI <ci@tcnp3.com>
2023-08-09 23:08:48 +0000

    update pytcnapi > adding newsroom initial changes

---
+++ api/v1alpha1/newsroom/service_pb2.pyi
@@ -0,0 +1,7 @@
+from annotations import authz_pb2 as _authz_pb2
+from api.v1alpha1.newsroom import entities_pb2 as _entities_pb2
+from google.api import annotations_pb2 as _annotations_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
