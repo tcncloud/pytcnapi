8078497d56d84841bec2c77c8309034688de0171
TCN CI <ci@tcnp3.com>
2023-08-30 21:08:16 +0000

    update pytcnapi > Merge branch 'master' of git.tcncloud.net:tcnpublic/api into 158-add-method-getwfmagentsid

--- api/commons/workflows/omni_pb2.pyi
+++ api/commons/workflows/omni_pb2.pyi
@@ -15,13 +15,41 @@ class OmniNodePrompt(_message.Message):
     options: _containers.RepeatedScalarFieldContainer[str]
     def __init__(self, prompt: _Optional[str] = ..., store_to: _Optional[str] = ..., options: _Optional[_Iterable[str]] = ...) -> None: ...
 
+class OmniNodeSendMessage(_message.Message):
+    __slots__ = ["prompt", "options"]
+    PROMPT_FIELD_NUMBER: _ClassVar[int]
+    OPTIONS_FIELD_NUMBER: _ClassVar[int]
+    prompt: str
+    options: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, prompt: _Optional[str] = ..., options: _Optional[_Iterable[str]] = ...) -> None: ...
+
+class OmniNodeUserInput(_message.Message):
+    __slots__ = ["store_id"]
+    STORE_ID_FIELD_NUMBER: _ClassVar[int]
+    store_id: str
+    def __init__(self, store_id: _Optional[str] = ...) -> None: ...
+
+class OmniNodeBranching(_message.Message):
+    __slots__ = ["store_id", "options"]
+    STORE_ID_FIELD_NUMBER: _ClassVar[int]
+    OPTIONS_FIELD_NUMBER: _ClassVar[int]
+    store_id: str
+    options: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, store_id: _Optional[str] = ..., options: _Optional[_Iterable[str]] = ...) -> None: ...
+
 class OmniNodeSetSkill(_message.Message):
-    __slots__ = ["skill"]
+    __slots__ = ["skill", "skills"]
     SKILL_FIELD_NUMBER: _ClassVar[int]
+    SKILLS_FIELD_NUMBER: _ClassVar[int]
     skill: str
-    def __init__(self, skill: _Optional[str] = ...) -> None: ...
+    skills: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, skill: _Optional[str] = ..., skills: _Optional[_Iterable[str]] = ...) -> None: ...
 
 class OmniNodeToAgent(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
+
+class OmniNodeToMatcher(_message.Message):
     __slots__ = []
     def __init__(self) -> None: ...
 
