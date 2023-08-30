7f8e64a5c3b4c4a818ee7912a971ac014c42a6e9
TCN CI <ci@tcnp3.com>
2023-08-30 21:31:57 +0000

    update pytcnapi > Merge branch '139-create-protos-for-sms-flows-integrations' into 'master'
    Resolve "Create Protos for SMS-Flows Integrations"
    
    Closes #139
    
    See merge request tcnpublic/api!154

--- api/commons/workflows/omni_pb2.pyi
+++ api/commons/workflows/omni_pb2.pyi
@@ -1,9 +1,17 @@
 from google.protobuf.internal import containers as _containers
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Optional
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
+
+class OmniNodeScrublistAction(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = []
+    ADD: _ClassVar[OmniNodeScrublistAction]
+    REMOVE: _ClassVar[OmniNodeScrublistAction]
+ADD: OmniNodeScrublistAction
+REMOVE: OmniNodeScrublistAction
 
 class OmniNodePrompt(_message.Message):
     __slots__ = ["prompt", "store_to", "options"]
@@ -58,3 +66,32 @@ class OmniNodeError(_message.Message):
     ERROR_FIELD_NUMBER: _ClassVar[int]
     error: str
     def __init__(self, error: _Optional[str] = ...) -> None: ...
+
+class OmniNodeWebhook(_message.Message):
+    __slots__ = ["url", "method", "body", "headers"]
+    class HeadersEntry(_message.Message):
+        __slots__ = ["key", "value"]
+        KEY_FIELD_NUMBER: _ClassVar[int]
+        VALUE_FIELD_NUMBER: _ClassVar[int]
+        key: str
+        value: str
+        def __init__(self, key: _Optional[str] = ..., value: _Optional[str] = ...) -> None: ...
+    URL_FIELD_NUMBER: _ClassVar[int]
+    METHOD_FIELD_NUMBER: _ClassVar[int]
+    BODY_FIELD_NUMBER: _ClassVar[int]
+    HEADERS_FIELD_NUMBER: _ClassVar[int]
+    url: str
+    method: str
+    body: str
+    headers: _containers.ScalarMap[str, str]
+    def __init__(self, url: _Optional[str] = ..., method: _Optional[str] = ..., body: _Optional[str] = ..., headers: _Optional[_Mapping[str, str]] = ...) -> None: ...
+
+class OmniNodeScrublist(_message.Message):
+    __slots__ = ["action"]
+    ACTION_FIELD_NUMBER: _ClassVar[int]
+    action: OmniNodeScrublistAction
+    def __init__(self, action: _Optional[_Union[OmniNodeScrublistAction, str]] = ...) -> None: ...
+
+class OmniNodeEndConversation(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
