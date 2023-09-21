b98b2e1eb352fe81cadecf033f859783fcfb5180
TCN CI <ci@tcnp3.com>
2023-09-21 20:28:07 +0000

    update pytcnapi > add lms_events file with pipeline failure

---
+++ api/commons/audit/lms_events_pb2.pyi
@@ -0,0 +1,16 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class LMSPipelineFailureEvent(_message.Message):
+    __slots__ = ["element_id", "element_name", "file_names"]
+    ELEMENT_ID_FIELD_NUMBER: _ClassVar[int]
+    ELEMENT_NAME_FIELD_NUMBER: _ClassVar[int]
+    FILE_NAMES_FIELD_NUMBER: _ClassVar[int]
+    element_id: str
+    element_name: str
+    file_names: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, element_id: _Optional[str] = ..., element_name: _Optional[str] = ..., file_names: _Optional[_Iterable[str]] = ...) -> None: ...
