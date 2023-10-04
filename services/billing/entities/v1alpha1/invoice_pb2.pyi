010128af386347b15640754838e74c99ffe0c930
TCN CI <ci@tcnp3.com>
2023-10-04 21:38:27 +0000

    update pytcnapi > refactor(billing): update resource ids

--- services/billing/entities/v1alpha1/invoice_pb2.pyi
+++ services/billing/entities/v1alpha1/invoice_pb2.pyi
@@ -9,9 +9,8 @@ from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Map
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class Invoice(_message.Message):
-    __slots__ = ["invoice_id", "org_id", "billing_cycle", "create_time", "update_time", "delete_time", "items", "url"]
+    __slots__ = ["invoice_id", "billing_cycle", "create_time", "update_time", "delete_time", "items", "url"]
     INVOICE_ID_FIELD_NUMBER: _ClassVar[int]
-    ORG_ID_FIELD_NUMBER: _ClassVar[int]
     BILLING_CYCLE_FIELD_NUMBER: _ClassVar[int]
     CREATE_TIME_FIELD_NUMBER: _ClassVar[int]
     UPDATE_TIME_FIELD_NUMBER: _ClassVar[int]
@@ -19,27 +18,24 @@ class Invoice(_message.Message):
     ITEMS_FIELD_NUMBER: _ClassVar[int]
     URL_FIELD_NUMBER: _ClassVar[int]
     invoice_id: str
-    org_id: str
     billing_cycle: str
     create_time: _timestamp_pb2.Timestamp
     update_time: _timestamp_pb2.Timestamp
     delete_time: _timestamp_pb2.Timestamp
     items: _containers.RepeatedCompositeFieldContainer[InvoiceItem]
     url: _wrappers_pb2.StringValue
-    def __init__(self, invoice_id: _Optional[str] = ..., org_id: _Optional[str] = ..., billing_cycle: _Optional[str] = ..., create_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., update_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., delete_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., items: _Optional[_Iterable[_Union[InvoiceItem, _Mapping]]] = ..., url: _Optional[_Union[_wrappers_pb2.StringValue, _Mapping]] = ...) -> None: ...
+    def __init__(self, invoice_id: _Optional[str] = ..., billing_cycle: _Optional[str] = ..., create_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., update_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., delete_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., items: _Optional[_Iterable[_Union[InvoiceItem, _Mapping]]] = ..., url: _Optional[_Union[_wrappers_pb2.StringValue, _Mapping]] = ...) -> None: ...
 
 class InvoiceItem(_message.Message):
-    __slots__ = ["invoice_item_id", "invoice_id", "product", "price", "create_time", "update_time"]
+    __slots__ = ["invoice_item_id", "product", "price", "create_time", "update_time"]
     INVOICE_ITEM_ID_FIELD_NUMBER: _ClassVar[int]
-    INVOICE_ID_FIELD_NUMBER: _ClassVar[int]
     PRODUCT_FIELD_NUMBER: _ClassVar[int]
     PRICE_FIELD_NUMBER: _ClassVar[int]
     CREATE_TIME_FIELD_NUMBER: _ClassVar[int]
     UPDATE_TIME_FIELD_NUMBER: _ClassVar[int]
     invoice_item_id: str
-    invoice_id: str
     product: _product_pb2.Product
     price: float
     create_time: _timestamp_pb2.Timestamp
     update_time: _timestamp_pb2.Timestamp
-    def __init__(self, invoice_item_id: _Optional[str] = ..., invoice_id: _Optional[str] = ..., product: _Optional[_Union[_product_pb2.Product, str]] = ..., price: _Optional[float] = ..., create_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., update_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ...) -> None: ...
+    def __init__(self, invoice_item_id: _Optional[str] = ..., product: _Optional[_Union[_product_pb2.Product, str]] = ..., price: _Optional[float] = ..., create_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., update_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ...) -> None: ...
--- services/billing/entities/v1alpha1/plan_pb2.pyi
+++ services/billing/entities/v1alpha1/plan_pb2.pyi
@@ -8,19 +8,17 @@ from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Map
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class BillingPlan(_message.Message):
-    __slots__ = ["billing_plan_id", "org_id", "create_time", "update_time", "start_time", "end_time", "rate_definitions"]
+    __slots__ = ["billing_plan_id", "create_time", "update_time", "start_time", "end_time", "rate_definitions"]
     BILLING_PLAN_ID_FIELD_NUMBER: _ClassVar[int]
-    ORG_ID_FIELD_NUMBER: _ClassVar[int]
     CREATE_TIME_FIELD_NUMBER: _ClassVar[int]
     UPDATE_TIME_FIELD_NUMBER: _ClassVar[int]
     START_TIME_FIELD_NUMBER: _ClassVar[int]
     END_TIME_FIELD_NUMBER: _ClassVar[int]
     RATE_DEFINITIONS_FIELD_NUMBER: _ClassVar[int]
     billing_plan_id: str
-    org_id: str
     create_time: _timestamp_pb2.Timestamp
     update_time: _timestamp_pb2.Timestamp
     start_time: _timestamp_pb2.Timestamp
     end_time: _timestamp_pb2.Timestamp
     rate_definitions: _containers.RepeatedCompositeFieldContainer[_rates_pb2.RateDefinition]
-    def __init__(self, billing_plan_id: _Optional[str] = ..., org_id: _Optional[str] = ..., create_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., update_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., start_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., end_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., rate_definitions: _Optional[_Iterable[_Union[_rates_pb2.RateDefinition, _Mapping]]] = ...) -> None: ...
+    def __init__(self, billing_plan_id: _Optional[str] = ..., create_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., update_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., start_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., end_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., rate_definitions: _Optional[_Iterable[_Union[_rates_pb2.RateDefinition, _Mapping]]] = ...) -> None: ...
