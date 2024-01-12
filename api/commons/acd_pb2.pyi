e9223536e144bfd3b238285ac00675182e01996d
TCN CI <ci@tcnp3.com>
2024-01-12 18:42:36 +0000

    update pytcnapi > add transfer member locatable data

--- api/commons/acd_pb2.pyi
+++ api/commons/acd_pb2.pyi
@@ -756,14 +756,20 @@ class ReplaceConfig(_message.Message):
     def __init__(self) -> None: ...
 
 class TransferMember(_message.Message):
-    __slots__ = ("identifier", "display_label", "member_type")
+    __slots__ = ("identifier", "display_label", "member_type", "agent_session", "caller_sid", "outbound_id")
     IDENTIFIER_FIELD_NUMBER: _ClassVar[int]
     DISPLAY_LABEL_FIELD_NUMBER: _ClassVar[int]
     MEMBER_TYPE_FIELD_NUMBER: _ClassVar[int]
+    AGENT_SESSION_FIELD_NUMBER: _ClassVar[int]
+    CALLER_SID_FIELD_NUMBER: _ClassVar[int]
+    OUTBOUND_ID_FIELD_NUMBER: _ClassVar[int]
     identifier: str
     display_label: str
     member_type: TransferMemberType
-    def __init__(self, identifier: _Optional[str] = ..., display_label: _Optional[str] = ..., member_type: _Optional[_Union[TransferMemberType, str]] = ...) -> None: ...
+    agent_session: AgentSession
+    caller_sid: CallerSid
+    outbound_id: str
+    def __init__(self, identifier: _Optional[str] = ..., display_label: _Optional[str] = ..., member_type: _Optional[_Union[TransferMemberType, str]] = ..., agent_session: _Optional[_Union[AgentSession, _Mapping]] = ..., caller_sid: _Optional[_Union[CallerSid, _Mapping]] = ..., outbound_id: _Optional[str] = ...) -> None: ...
 
 class AgentAlert(_message.Message):
     __slots__ = ("backoffice_message", "directed_call_ringing", "directed_call_hangup")
