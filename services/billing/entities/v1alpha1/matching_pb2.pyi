70e1a54c473dbbf56521b30f6b8c07c230d9bfe1
TCN CI <ci@tcnp3.com>
2024-01-18 21:38:04 +0000

    update pytcnapi > refactor(billing): update matching config and remove feature/group ids

--- services/billing/entities/v1alpha1/matching_pb2.pyi
+++ services/billing/entities/v1alpha1/matching_pb2.pyi
@@ -10,14 +10,20 @@ class MatchingRule(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     __slots__ = ()
     MATCHING_RULE_UNSPECIFIED: _ClassVar[MatchingRule]
     MATCHING_RULE_AREA_CODE: _ClassVar[MatchingRule]
+    MATCHING_RULE_COUNTRY_PREFIX: _ClassVar[MatchingRule]
 MATCHING_RULE_UNSPECIFIED: MatchingRule
 MATCHING_RULE_AREA_CODE: MatchingRule
+MATCHING_RULE_COUNTRY_PREFIX: MatchingRule
 
 class MatchingConfig(_message.Message):
-    __slots__ = ("area_code",)
+    __slots__ = ("name", "area_code", "country_prefix")
+    NAME_FIELD_NUMBER: _ClassVar[int]
     AREA_CODE_FIELD_NUMBER: _ClassVar[int]
+    COUNTRY_PREFIX_FIELD_NUMBER: _ClassVar[int]
+    name: str
     area_code: MatchingConfigAreaCode
-    def __init__(self, area_code: _Optional[_Union[MatchingConfigAreaCode, _Mapping]] = ...) -> None: ...
+    country_prefix: MatchingConfigCountryPrefix
+    def __init__(self, name: _Optional[str] = ..., area_code: _Optional[_Union[MatchingConfigAreaCode, _Mapping]] = ..., country_prefix: _Optional[_Union[MatchingConfigCountryPrefix, _Mapping]] = ...) -> None: ...
 
 class MatchingConfigAreaCode(_message.Message):
     __slots__ = ("name", "area_codes")
@@ -26,3 +32,11 @@ class MatchingConfigAreaCode(_message.Message):
     name: str
     area_codes: _containers.RepeatedScalarFieldContainer[str]
     def __init__(self, name: _Optional[str] = ..., area_codes: _Optional[_Iterable[str]] = ...) -> None: ...
+
+class MatchingConfigCountryPrefix(_message.Message):
+    __slots__ = ("country_code", "prefixes")
+    COUNTRY_CODE_FIELD_NUMBER: _ClassVar[int]
+    PREFIXES_FIELD_NUMBER: _ClassVar[int]
+    country_code: int
+    prefixes: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, country_code: _Optional[int] = ..., prefixes: _Optional[_Iterable[str]] = ...) -> None: ...
