727bb67cc49fde3b624491fac7d68e77e5c6f1c8
TCN CI <ci@tcnp3.com>
2024-02-04 03:01:33 +0000

    update pytcnapi > Merge branch '367-add-delivery-failure-success-event-and-event-types' into 'master'
    Resolve "add delivery failure/success event and event types"
    
    Closes #367
    
    See merge request tcnpublic/api!391

---
+++ api/commons/audit/delivery_events_pb2.pyi
@@ -0,0 +1,43 @@
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class DeliveryFailureEvent(_message.Message):
+    __slots__ = ("delivery_definition_name", "org_id", "transaction_sid", "attachment_names", "failure_time", "failure_error_message", "definition", "original_payload")
+    DELIVERY_DEFINITION_NAME_FIELD_NUMBER: _ClassVar[int]
+    ORG_ID_FIELD_NUMBER: _ClassVar[int]
+    TRANSACTION_SID_FIELD_NUMBER: _ClassVar[int]
+    ATTACHMENT_NAMES_FIELD_NUMBER: _ClassVar[int]
+    FAILURE_TIME_FIELD_NUMBER: _ClassVar[int]
+    FAILURE_ERROR_MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    DEFINITION_FIELD_NUMBER: _ClassVar[int]
+    ORIGINAL_PAYLOAD_FIELD_NUMBER: _ClassVar[int]
+    delivery_definition_name: int
+    org_id: str
+    transaction_sid: int
+    attachment_names: _containers.RepeatedScalarFieldContainer[str]
+    failure_time: _timestamp_pb2.Timestamp
+    failure_error_message: str
+    definition: str
+    original_payload: str
+    def __init__(self, delivery_definition_name: _Optional[int] = ..., org_id: _Optional[str] = ..., transaction_sid: _Optional[int] = ..., attachment_names: _Optional[_Iterable[str]] = ..., failure_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., failure_error_message: _Optional[str] = ..., definition: _Optional[str] = ..., original_payload: _Optional[str] = ...) -> None: ...
+
+class DeliverySuccessEvent(_message.Message):
+    __slots__ = ("delivery_definition_name", "org_id", "transaction_sid", "attachment_names", "success_time", "success_message")
+    DELIVERY_DEFINITION_NAME_FIELD_NUMBER: _ClassVar[int]
+    ORG_ID_FIELD_NUMBER: _ClassVar[int]
+    TRANSACTION_SID_FIELD_NUMBER: _ClassVar[int]
+    ATTACHMENT_NAMES_FIELD_NUMBER: _ClassVar[int]
+    SUCCESS_TIME_FIELD_NUMBER: _ClassVar[int]
+    SUCCESS_MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    delivery_definition_name: str
+    org_id: str
+    transaction_sid: int
+    attachment_names: _containers.RepeatedScalarFieldContainer[str]
+    success_time: _timestamp_pb2.Timestamp
+    success_message: str
+    def __init__(self, delivery_definition_name: _Optional[str] = ..., org_id: _Optional[str] = ..., transaction_sid: _Optional[int] = ..., attachment_names: _Optional[_Iterable[str]] = ..., success_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., success_message: _Optional[str] = ...) -> None: ...
