c65a287106f9f4fced359b57c22ebe559b449cd7
TCN CI <ci@tcnp3.com>
2024-02-22 22:48:46 +0000

    update pytcnapi > Merge branch '406-add-mfa-fields-to-listagentsresponse-agentdetails' into 'master'
    Resolve "Add MFA fields to ListAgentsResponse.AgentDetails"
    
    Closes #406
    
    See merge request tcnpublic/api!428

--- api/v1alpha1/org/user_pb2.pyi
+++ api/v1alpha1/org/user_pb2.pyi
@@ -279,7 +279,7 @@ class ListAgentsRequest(_message.Message):
 class ListAgentsResponse(_message.Message):
     __slots__ = ("agents",)
     class AgentDetails(_message.Message):
-        __slots__ = ("user_id", "org_id", "first_name", "last_name", "username", "skills", "login_disabled", "hunt_group", "labels", "delegated", "trust_ids", "permission_groups", "agent_sid", "name", "partner_agent_id", "user_caller_id", "created", "last_updated", "agent_profile_group", "agent", "time_zone_override", "email")
+        __slots__ = ("user_id", "org_id", "first_name", "last_name", "username", "skills", "login_disabled", "hunt_group", "labels", "delegated", "trust_ids", "permission_groups", "agent_sid", "name", "partner_agent_id", "user_caller_id", "created", "last_updated", "agent_profile_group", "agent", "time_zone_override", "email", "email_verified", "mfa_info")
         class HuntGroup(_message.Message):
             __slots__ = ("hunt_group_sid", "hunt_group_name")
             HUNT_GROUP_SID_FIELD_NUMBER: _ClassVar[int]
@@ -316,6 +316,8 @@ class ListAgentsResponse(_message.Message):
         AGENT_FIELD_NUMBER: _ClassVar[int]
         TIME_ZONE_OVERRIDE_FIELD_NUMBER: _ClassVar[int]
         EMAIL_FIELD_NUMBER: _ClassVar[int]
+        EMAIL_VERIFIED_FIELD_NUMBER: _ClassVar[int]
+        MFA_INFO_FIELD_NUMBER: _ClassVar[int]
         user_id: str
         org_id: str
         first_name: str
@@ -338,7 +340,9 @@ class ListAgentsResponse(_message.Message):
         agent: bool
         time_zone_override: _org_pb2.TimeZoneWrapper
         email: str
-        def __init__(self, user_id: _Optional[str] = ..., org_id: _Optional[str] = ..., first_name: _Optional[str] = ..., last_name: _Optional[str] = ..., username: _Optional[str] = ..., skills: _Optional[_Iterable[_Union[_user_pb2.Skill, _Mapping]]] = ..., login_disabled: bool = ..., hunt_group: _Optional[_Union[ListAgentsResponse.AgentDetails.HuntGroup, _Mapping]] = ..., labels: _Optional[_Iterable[_Union[_labels_pb2.Label, _Mapping]]] = ..., delegated: bool = ..., trust_ids: _Optional[_Iterable[str]] = ..., permission_groups: _Optional[_Iterable[_Union[_permissions_pb2.PermissionGroup, _Mapping]]] = ..., agent_sid: _Optional[int] = ..., name: _Optional[str] = ..., partner_agent_id: _Optional[str] = ..., user_caller_id: _Optional[str] = ..., created: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., last_updated: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., agent_profile_group: _Optional[_Union[ListAgentsResponse.AgentDetails.AgentProfileGroup, _Mapping]] = ..., agent: bool = ..., time_zone_override: _Optional[_Union[_org_pb2.TimeZoneWrapper, _Mapping]] = ..., email: _Optional[str] = ...) -> None: ...
+        email_verified: bool
+        mfa_info: _user_pb2.MfaInfo
+        def __init__(self, user_id: _Optional[str] = ..., org_id: _Optional[str] = ..., first_name: _Optional[str] = ..., last_name: _Optional[str] = ..., username: _Optional[str] = ..., skills: _Optional[_Iterable[_Union[_user_pb2.Skill, _Mapping]]] = ..., login_disabled: bool = ..., hunt_group: _Optional[_Union[ListAgentsResponse.AgentDetails.HuntGroup, _Mapping]] = ..., labels: _Optional[_Iterable[_Union[_labels_pb2.Label, _Mapping]]] = ..., delegated: bool = ..., trust_ids: _Optional[_Iterable[str]] = ..., permission_groups: _Optional[_Iterable[_Union[_permissions_pb2.PermissionGroup, _Mapping]]] = ..., agent_sid: _Optional[int] = ..., name: _Optional[str] = ..., partner_agent_id: _Optional[str] = ..., user_caller_id: _Optional[str] = ..., created: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., last_updated: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., agent_profile_group: _Optional[_Union[ListAgentsResponse.AgentDetails.AgentProfileGroup, _Mapping]] = ..., agent: bool = ..., time_zone_override: _Optional[_Union[_org_pb2.TimeZoneWrapper, _Mapping]] = ..., email: _Optional[str] = ..., email_verified: bool = ..., mfa_info: _Optional[_Union[_user_pb2.MfaInfo, _Mapping]] = ...) -> None: ...
     AGENTS_FIELD_NUMBER: _ClassVar[int]
     agents: _containers.RepeatedCompositeFieldContainer[ListAgentsResponse.AgentDetails]
     def __init__(self, agents: _Optional[_Iterable[_Union[ListAgentsResponse.AgentDetails, _Mapping]]] = ...) -> None: ...
