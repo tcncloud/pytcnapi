355a464dcd20c1ea1b5cff3131b91f245ba3283c
TCN CI <ci@tcnp3.com>
2024-03-04 21:07:15 +0000

    update pytcnapi > Updates to learn endpoint

--- api/v0alpha/learn_pb2.pyi
+++ api/v0alpha/learn_pb2.pyi
@@ -316,14 +316,12 @@ class ReviewFileVersionsRes(_message.Message):
     def __init__(self, content: _Optional[str] = ..., diff_content: _Optional[str] = ..., images: _Optional[_Iterable[_Union[LearnImage, _Mapping]]] = ...) -> None: ...
 
 class ReviewVersionReq(_message.Message):
-    __slots__ = ("url", "version", "locale")
-    URL_FIELD_NUMBER: _ClassVar[int]
+    __slots__ = ("version", "locale")
     VERSION_FIELD_NUMBER: _ClassVar[int]
     LOCALE_FIELD_NUMBER: _ClassVar[int]
-    url: str
     version: str
     locale: str
-    def __init__(self, url: _Optional[str] = ..., version: _Optional[str] = ..., locale: _Optional[str] = ...) -> None: ...
+    def __init__(self, version: _Optional[str] = ..., locale: _Optional[str] = ...) -> None: ...
 
 class ReviewVersionRes(_message.Message):
     __slots__ = ("diff_urls", "diff_file_names")
