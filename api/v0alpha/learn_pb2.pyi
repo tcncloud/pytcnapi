a0ee4a816a180c60c0f9630416f48a1259aeea35
TCN CI <ci@tcnp3.com>
2024-03-05 21:08:55 +0000

    update pytcnapi > Merge branch '420-learn-add-reviewversion-api-call' into 'master'
    Resolve "Learn -  Add ReviewVersion api call"
    
    Closes #420
    
    See merge request tcnpublic/api!442

--- api/v0alpha/learn_pb2.pyi
+++ api/v0alpha/learn_pb2.pyi
@@ -314,3 +314,19 @@ class ReviewFileVersionsRes(_message.Message):
     diff_content: str
     images: _containers.RepeatedCompositeFieldContainer[LearnImage]
     def __init__(self, content: _Optional[str] = ..., diff_content: _Optional[str] = ..., images: _Optional[_Iterable[_Union[LearnImage, _Mapping]]] = ...) -> None: ...
+
+class ReviewVersionReq(_message.Message):
+    __slots__ = ("version", "locale")
+    VERSION_FIELD_NUMBER: _ClassVar[int]
+    LOCALE_FIELD_NUMBER: _ClassVar[int]
+    version: str
+    locale: str
+    def __init__(self, version: _Optional[str] = ..., locale: _Optional[str] = ...) -> None: ...
+
+class ReviewVersionRes(_message.Message):
+    __slots__ = ("diff_urls", "diff_file_names")
+    DIFF_URLS_FIELD_NUMBER: _ClassVar[int]
+    DIFF_FILE_NAMES_FIELD_NUMBER: _ClassVar[int]
+    diff_urls: str
+    diff_file_names: str
+    def __init__(self, diff_urls: _Optional[str] = ..., diff_file_names: _Optional[str] = ...) -> None: ...
