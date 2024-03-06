6e918acf9654c619c306471240749d9b554b783c
TCN CI <ci@tcnp3.com>
2024-03-06 20:58:16 +0000

    update pytcnapi > add enum

--- annotations/perms/tcn_pb2.pyi
+++ annotations/perms/tcn_pb2.pyi
@@ -1,11 +1,25 @@
 from annotations.perms import license_pb2 as _license_pb2
 from google.protobuf import descriptor_pb2 as _descriptor_pb2
 from google.protobuf.internal import containers as _containers
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
+
+class DefaultPermissionGroup(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = ()
+    ACCOUNT_OWNER_GROUP: _ClassVar[DefaultPermissionGroup]
+    SUPER_USER_GROUP: _ClassVar[DefaultPermissionGroup]
+    USER_GROUP: _ClassVar[DefaultPermissionGroup]
+    MONITOR_GROUP: _ClassVar[DefaultPermissionGroup]
+    AGENT_GROUP: _ClassVar[DefaultPermissionGroup]
+ACCOUNT_OWNER_GROUP: DefaultPermissionGroup
+SUPER_USER_GROUP: DefaultPermissionGroup
+USER_GROUP: DefaultPermissionGroup
+MONITOR_GROUP: DefaultPermissionGroup
+AGENT_GROUP: DefaultPermissionGroup
 OPTIONS_FIELD_NUMBER: _ClassVar[int]
 options: _descriptor.FieldDescriptor
 
