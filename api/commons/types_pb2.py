abc63db823155a12889d6ddd5203a5b17ee5e516
TCN CI <ci@tcnp3.com>
2024-03-09 17:54:04 +0000

    update pytcnapi > Merge branch '422-bi-report-generator-redo-protos-for-new-implementation' into 'master'
    Resolve "BI Report Generator: Redo Protos For New Implementation"
    
    Closes #422
    
    See merge request tcnpublic/api!444

--- api/commons/types_pb2.py
+++ api/commons/types_pb2.py
@@ -16,7 +16,7 @@ from google.protobuf import timestamp_pb2 as google_dot_protobuf_dot_timestamp__
 from google.protobuf import wrappers_pb2 as google_dot_protobuf_dot_wrappers__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17\x61pi/commons/types.proto\x12\x0b\x61pi.commons\x1a\x1fgoogle/protobuf/timestamp.proto\x1a\x1egoogle/protobuf/wrappers.proto\"\x07\n\x05\x45mpty\">\n\rInt32Nullable\x12\x17\n\x07is_null\x18\x01 \x01(\x08R\x06isNull\x12\x14\n\x05value\x18\x02 \x01(\x05R\x05value\">\n\rInt64Nullable\x12\x17\n\x07is_null\x18\x01 \x01(\x08R\x06isNull\x12\x14\n\x05value\x18\x02 \x01(\x03R\x05value\"\xaa\x01\n\x15SomeSidAndDateCompare\x12\x19\n\x08some_sid\x18\x01 \x01(\x03R\x07someSid\x12=\n\x0c\x64\x61te_greater\x18\x02 \x01(\x0b\x32\x1a.google.protobuf.TimestampR\x0b\x64\x61teGreater\x12\x37\n\tdate_less\x18\x03 \x01(\x0b\x32\x1a.google.protobuf.TimestampR\x08\x64\x61teLess\"\'\n\rInt64ArraySql\x12\x16\n\x06values\x18\x01 \x03(\x03R\x06values\"\'\n\rInt32ArraySql\x12\x16\n\x06values\x18\x01 \x03(\x05R\x06values\"(\n\x0eStringArraySql\x12\x16\n\x06values\x18\x02 \x03(\tR\x06values\"&\n\x0c\x42oolArraySql\x12\x16\n\x06values\x18\x01 \x03(\x08R\x06values\"I\n\x12Int32ValueArraySql\x12\x33\n\x06values\x18\x01 \x03(\x0b\x32\x1b.google.protobuf.Int32ValueR\x06values\"#\n\x07Int64Id\x12\x18\n\x05value\x18\x01 \x01(\x03\x42\x02\x30\x01R\x05valueBj\n\x0f\x63om.api.commonsB\nTypesProtoP\x01\xa2\x02\x03\x41\x43X\xaa\x02\x0b\x41pi.Commons\xca\x02\x0b\x41pi\\Commons\xe2\x02\x17\x41pi\\Commons\\GPBMetadata\xea\x02\x0c\x41pi::Commonsb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17\x61pi/commons/types.proto\x12\x0b\x61pi.commons\x1a\x1fgoogle/protobuf/timestamp.proto\x1a\x1egoogle/protobuf/wrappers.proto\"\x07\n\x05\x45mpty\"\xc3\x01\n\x0e\x43ronExpression\x12%\n\x0erepeat_minutes\x18\x01 \x01(\tR\rrepeatMinutes\x12 \n\x0chours_of_day\x18\x02 \x01(\tR\nhoursOfDay\x12\"\n\rdays_of_month\x18\x03 \x01(\tR\x0b\x64\x61ysOfMonth\x12$\n\x0emonths_of_year\x18\x04 \x01(\tR\x0cmonthsOfYear\x12\x1e\n\x0b\x64\x61y_of_week\x18\x05 \x01(\tR\tdayOfWeek\">\n\rInt32Nullable\x12\x17\n\x07is_null\x18\x01 \x01(\x08R\x06isNull\x12\x14\n\x05value\x18\x02 \x01(\x05R\x05value\">\n\rInt64Nullable\x12\x17\n\x07is_null\x18\x01 \x01(\x08R\x06isNull\x12\x14\n\x05value\x18\x02 \x01(\x03R\x05value\"\xaa\x01\n\x15SomeSidAndDateCompare\x12\x19\n\x08some_sid\x18\x01 \x01(\x03R\x07someSid\x12=\n\x0c\x64\x61te_greater\x18\x02 \x01(\x0b\x32\x1a.google.protobuf.TimestampR\x0b\x64\x61teGreater\x12\x37\n\tdate_less\x18\x03 \x01(\x0b\x32\x1a.google.protobuf.TimestampR\x08\x64\x61teLess\"\'\n\rInt64ArraySql\x12\x16\n\x06values\x18\x01 \x03(\x03R\x06values\"\'\n\rInt32ArraySql\x12\x16\n\x06values\x18\x01 \x03(\x05R\x06values\"(\n\x0eStringArraySql\x12\x16\n\x06values\x18\x02 \x03(\tR\x06values\"&\n\x0c\x42oolArraySql\x12\x16\n\x06values\x18\x01 \x03(\x08R\x06values\"I\n\x12Int32ValueArraySql\x12\x33\n\x06values\x18\x01 \x03(\x0b\x32\x1b.google.protobuf.Int32ValueR\x06values\"#\n\x07Int64Id\x12\x18\n\x05value\x18\x01 \x01(\x03\x42\x02\x30\x01R\x05valueBj\n\x0f\x63om.api.commonsB\nTypesProtoP\x01\xa2\x02\x03\x41\x43X\xaa\x02\x0b\x41pi.Commons\xca\x02\x0b\x41pi\\Commons\xe2\x02\x17\x41pi\\Commons\\GPBMetadata\xea\x02\x0c\x41pi::Commonsb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -28,22 +28,24 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _globals['_INT64ID'].fields_by_name['value']._serialized_options = b'0\001'
   _globals['_EMPTY']._serialized_start=105
   _globals['_EMPTY']._serialized_end=112
-  _globals['_INT32NULLABLE']._serialized_start=114
-  _globals['_INT32NULLABLE']._serialized_end=176
-  _globals['_INT64NULLABLE']._serialized_start=178
-  _globals['_INT64NULLABLE']._serialized_end=240
-  _globals['_SOMESIDANDDATECOMPARE']._serialized_start=243
-  _globals['_SOMESIDANDDATECOMPARE']._serialized_end=413
-  _globals['_INT64ARRAYSQL']._serialized_start=415
-  _globals['_INT64ARRAYSQL']._serialized_end=454
-  _globals['_INT32ARRAYSQL']._serialized_start=456
-  _globals['_INT32ARRAYSQL']._serialized_end=495
-  _globals['_STRINGARRAYSQL']._serialized_start=497
-  _globals['_STRINGARRAYSQL']._serialized_end=537
-  _globals['_BOOLARRAYSQL']._serialized_start=539
-  _globals['_BOOLARRAYSQL']._serialized_end=577
-  _globals['_INT32VALUEARRAYSQL']._serialized_start=579
-  _globals['_INT32VALUEARRAYSQL']._serialized_end=652
-  _globals['_INT64ID']._serialized_start=654
-  _globals['_INT64ID']._serialized_end=689
+  _globals['_CRONEXPRESSION']._serialized_start=115
+  _globals['_CRONEXPRESSION']._serialized_end=310
+  _globals['_INT32NULLABLE']._serialized_start=312
+  _globals['_INT32NULLABLE']._serialized_end=374
+  _globals['_INT64NULLABLE']._serialized_start=376
+  _globals['_INT64NULLABLE']._serialized_end=438
+  _globals['_SOMESIDANDDATECOMPARE']._serialized_start=441
+  _globals['_SOMESIDANDDATECOMPARE']._serialized_end=611
+  _globals['_INT64ARRAYSQL']._serialized_start=613
+  _globals['_INT64ARRAYSQL']._serialized_end=652
+  _globals['_INT32ARRAYSQL']._serialized_start=654
+  _globals['_INT32ARRAYSQL']._serialized_end=693
+  _globals['_STRINGARRAYSQL']._serialized_start=695
+  _globals['_STRINGARRAYSQL']._serialized_end=735
+  _globals['_BOOLARRAYSQL']._serialized_start=737
+  _globals['_BOOLARRAYSQL']._serialized_end=775
+  _globals['_INT32VALUEARRAYSQL']._serialized_start=777
+  _globals['_INT32VALUEARRAYSQL']._serialized_end=850
+  _globals['_INT64ID']._serialized_start=852
+  _globals['_INT64ID']._serialized_end=887
 # @@protoc_insertion_point(module_scope)
--- api/v1alpha1/bireportgenerator/entities_pb2.pyi
+++ api/v1alpha1/bireportgenerator/entities_pb2.pyi
@@ -1,6 +1,7 @@
 from api.commons import bireportgenerator_pb2 as _bireportgenerator_pb2
 from api.commons import enums_pb2 as _enums_pb2
 from api.commons import org_pb2 as _org_pb2
+from api.commons import types_pb2 as _types_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
@@ -9,7 +10,7 @@ from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Map
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class ReportJob(_message.Message):
-    __slots__ = ("report_job_id", "name", "description", "dashboard_id", "time_zone", "time_period", "delivery_times", "day_filter", "months", "format_options", "delivery_options", "is_active", "send_empty_report", "dashboard_resource_id", "time_zone_wrapper", "hide_csv_footer")
+    __slots__ = ("report_job_id", "name", "description", "dashboard_id", "time_zone", "time_period", "delivery_times", "day_filter", "months", "format_options", "delivery_options", "is_active", "send_empty_report", "dashboard_resource_id", "time_zone_wrapper", "hide_csv_footer", "transfer_config_sid", "cron_expression")
     REPORT_JOB_ID_FIELD_NUMBER: _ClassVar[int]
     NAME_FIELD_NUMBER: _ClassVar[int]
     DESCRIPTION_FIELD_NUMBER: _ClassVar[int]
@@ -26,6 +27,8 @@ class ReportJob(_message.Message):
     DASHBOARD_RESOURCE_ID_FIELD_NUMBER: _ClassVar[int]
     TIME_ZONE_WRAPPER_FIELD_NUMBER: _ClassVar[int]
     HIDE_CSV_FOOTER_FIELD_NUMBER: _ClassVar[int]
+    TRANSFER_CONFIG_SID_FIELD_NUMBER: _ClassVar[int]
+    CRON_EXPRESSION_FIELD_NUMBER: _ClassVar[int]
     report_job_id: str
     name: str
     description: str
@@ -42,4 +45,6 @@ class ReportJob(_message.Message):
     dashboard_resource_id: str
     time_zone_wrapper: _org_pb2.TimeZoneWrapper
     hide_csv_footer: bool
-    def __init__(self, report_job_id: _Optional[str] = ..., name: _Optional[str] = ..., description: _Optional[str] = ..., dashboard_id: _Optional[str] = ..., time_zone: _Optional[str] = ..., time_period: _Optional[_Union[_bireportgenerator_pb2.TimePeriod, str]] = ..., delivery_times: _Optional[_Union[_bireportgenerator_pb2.DeliveryTimes, _Mapping]] = ..., day_filter: _Optional[_Union[_bireportgenerator_pb2.DayFilter, _Mapping]] = ..., months: _Optional[_Iterable[_Union[_enums_pb2.Month, str]]] = ..., format_options: _Optional[_Union[_bireportgenerator_pb2.FormatOptions, _Mapping]] = ..., delivery_options: _Optional[_Union[_bireportgenerator_pb2.DeliveryOptions, _Mapping]] = ..., is_active: bool = ..., send_empty_report: bool = ..., dashboard_resource_id: _Optional[str] = ..., time_zone_wrapper: _Optional[_Union[_org_pb2.TimeZoneWrapper, _Mapping]] = ..., hide_csv_footer: bool = ...) -> None: ...
+    transfer_config_sid: int
+    cron_expression: _types_pb2.CronExpression
+    def __init__(self, report_job_id: _Optional[str] = ..., name: _Optional[str] = ..., description: _Optional[str] = ..., dashboard_id: _Optional[str] = ..., time_zone: _Optional[str] = ..., time_period: _Optional[_Union[_bireportgenerator_pb2.TimePeriod, str]] = ..., delivery_times: _Optional[_Union[_bireportgenerator_pb2.DeliveryTimes, _Mapping]] = ..., day_filter: _Optional[_Union[_bireportgenerator_pb2.DayFilter, _Mapping]] = ..., months: _Optional[_Iterable[_Union[_enums_pb2.Month, str]]] = ..., format_options: _Optional[_Union[_bireportgenerator_pb2.FormatOptions, _Mapping]] = ..., delivery_options: _Optional[_Union[_bireportgenerator_pb2.DeliveryOptions, _Mapping]] = ..., is_active: bool = ..., send_empty_report: bool = ..., dashboard_resource_id: _Optional[str] = ..., time_zone_wrapper: _Optional[_Union[_org_pb2.TimeZoneWrapper, _Mapping]] = ..., hide_csv_footer: bool = ..., transfer_config_sid: _Optional[int] = ..., cron_expression: _Optional[_Union[_types_pb2.CronExpression, _Mapping]] = ...) -> None: ...
