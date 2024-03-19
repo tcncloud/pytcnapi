b38a20721e48367f44a6917fae5c84a322ff0de6
TCN CI <ci@tcnp3.com>
2024-03-19 19:28:43 +0000

    update pytcnapi > adding permissions group to rooms

--- api/commons/room303_pb2.pyi
+++ api/commons/room303_pb2.pyi
@@ -69,7 +69,7 @@ class Member(_message.Message):
     def __init__(self, user_sid: _Optional[_Union[UserSid, _Mapping]] = ..., added_by: _Optional[_Union[UserSid, _Mapping]] = ..., added_at: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., room_id: _Optional[str] = ..., admin: bool = ...) -> None: ...
 
 class Room(_message.Message):
-    __slots__ = ("org_id", "room_id", "type", "created_at", "updated_at", "status", "id", "display_name", "config")
+    __slots__ = ("org_id", "room_id", "type", "created_at", "updated_at", "status", "id", "display_name", "config", "permission_group_id")
     ORG_ID_FIELD_NUMBER: _ClassVar[int]
     ROOM_ID_FIELD_NUMBER: _ClassVar[int]
     TYPE_FIELD_NUMBER: _ClassVar[int]
@@ -79,6 +79,7 @@ class Room(_message.Message):
     ID_FIELD_NUMBER: _ClassVar[int]
     DISPLAY_NAME_FIELD_NUMBER: _ClassVar[int]
     CONFIG_FIELD_NUMBER: _ClassVar[int]
+    PERMISSION_GROUP_ID_FIELD_NUMBER: _ClassVar[int]
     org_id: str
     room_id: str
     type: RoomType
@@ -88,7 +89,8 @@ class Room(_message.Message):
     id: str
     display_name: str
     config: RoomConfig
-    def __init__(self, org_id: _Optional[str] = ..., room_id: _Optional[str] = ..., type: _Optional[_Union[RoomType, str]] = ..., created_at: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., updated_at: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., status: _Optional[_Union[RoomStatus, str]] = ..., id: _Optional[str] = ..., display_name: _Optional[str] = ..., config: _Optional[_Union[RoomConfig, _Mapping]] = ...) -> None: ...
+    permission_group_id: str
+    def __init__(self, org_id: _Optional[str] = ..., room_id: _Optional[str] = ..., type: _Optional[_Union[RoomType, str]] = ..., created_at: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., updated_at: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., status: _Optional[_Union[RoomStatus, str]] = ..., id: _Optional[str] = ..., display_name: _Optional[str] = ..., config: _Optional[_Union[RoomConfig, _Mapping]] = ..., permission_group_id: _Optional[str] = ...) -> None: ...
 
 class Message(_message.Message):
     __slots__ = ("org_id", "message_id", "room_id", "from_user", "status", "received_at", "updated_at", "payload", "unread", "nonce")
