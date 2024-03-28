919549680e52102884746d63c2cc0e92ee88c729
TCN CI <ci@tcnp3.com>
2024-03-28 18:45:48 +0000

    update pytcnapi > CreateRoom updates

--- api/v1alpha1/room303/room_pb2.pyi
+++ api/v1alpha1/room303/room_pb2.pyi
@@ -2,6 +2,7 @@ from api.commons import room303_pb2 as _room303_pb2
 from api.commons import user_pb2 as _user_pb2
 from google.protobuf import field_mask_pb2 as _field_mask_pb2
 from google.protobuf import timestamp_pb2 as _timestamp_pb2
+from google.protobuf import wrappers_pb2 as _wrappers_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
@@ -10,14 +11,16 @@ from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Map
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class CreateRoomRequest(_message.Message):
-    __slots__ = ("name", "type", "members")
+    __slots__ = ("name", "type", "members", "permission_group_id")
     NAME_FIELD_NUMBER: _ClassVar[int]
     TYPE_FIELD_NUMBER: _ClassVar[int]
     MEMBERS_FIELD_NUMBER: _ClassVar[int]
+    PERMISSION_GROUP_ID_FIELD_NUMBER: _ClassVar[int]
     name: str
     type: _room303_pb2.RoomType
     members: _containers.RepeatedScalarFieldContainer[str]
-    def __init__(self, name: _Optional[str] = ..., type: _Optional[_Union[_room303_pb2.RoomType, str]] = ..., members: _Optional[_Iterable[str]] = ...) -> None: ...
+    permission_group_id: _wrappers_pb2.StringValue
+    def __init__(self, name: _Optional[str] = ..., type: _Optional[_Union[_room303_pb2.RoomType, str]] = ..., members: _Optional[_Iterable[str]] = ..., permission_group_id: _Optional[_Union[_wrappers_pb2.StringValue, _Mapping]] = ...) -> None: ...
 
 class GetRoomRequest(_message.Message):
     __slots__ = ("room_id",)
