1eace8736e729b0a93edda607f1bcc0247211095
TCN CI <ci@tcnp3.com>
2024-04-02 20:15:01 +0000

    update pytcnapi > Merge branch '470-add-whitelisted-ips-to-user-entity' into 'master'
    Resolve "Add whitelisted IPs to user entity"
    
    Closes #470
    
    See merge request tcnpublic/api!496

--- api/commons/org/user_pb2.pyi
+++ api/commons/org/user_pb2.pyi
@@ -9,7 +9,7 @@ from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Map
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class User(_message.Message):
-    __slots__ = ("user_id", "org_id", "username", "p3_permission_group_id", "login_sid", "agent_sid", "region_id", "partner_agent_id", "client_sid", "region_sid_map", "api_key", "email", "login_disabled", "caller_ids", "linkback_numbers", "auth_user_id", "enable_mfa", "first_name", "last_name", "created", "last_updated", "password_reset_required", "connection_id", "time_zone_override", "permission_group_ids", "trust_ids", "default_region", "default_application", "user_caller_id", "agent_profile_group_id", "skills", "agent", "account_owner", "email_verified")
+    __slots__ = ("user_id", "org_id", "username", "p3_permission_group_id", "login_sid", "agent_sid", "region_id", "partner_agent_id", "client_sid", "region_sid_map", "api_key", "email", "login_disabled", "caller_ids", "linkback_numbers", "auth_user_id", "enable_mfa", "first_name", "last_name", "created", "last_updated", "password_reset_required", "connection_id", "time_zone_override", "permission_group_ids", "trust_ids", "default_region", "default_application", "user_caller_id", "agent_profile_group_id", "skills", "agent", "account_owner", "email_verified", "whitelisted_ips")
     class RegionSidMapEntry(_message.Message):
         __slots__ = ("key", "value")
         KEY_FIELD_NUMBER: _ClassVar[int]
@@ -60,6 +60,7 @@ class User(_message.Message):
     AGENT_FIELD_NUMBER: _ClassVar[int]
     ACCOUNT_OWNER_FIELD_NUMBER: _ClassVar[int]
     EMAIL_VERIFIED_FIELD_NUMBER: _ClassVar[int]
+    WHITELISTED_IPS_FIELD_NUMBER: _ClassVar[int]
     user_id: str
     org_id: str
     username: str
@@ -94,7 +95,8 @@ class User(_message.Message):
     agent: bool
     account_owner: bool
     email_verified: bool
-    def __init__(self, user_id: _Optional[str] = ..., org_id: _Optional[str] = ..., username: _Optional[str] = ..., p3_permission_group_id: _Optional[str] = ..., login_sid: _Optional[int] = ..., agent_sid: _Optional[int] = ..., region_id: _Optional[str] = ..., partner_agent_id: _Optional[str] = ..., client_sid: _Optional[int] = ..., region_sid_map: _Optional[_Mapping[str, User.RegionSids]] = ..., api_key: _Optional[str] = ..., email: _Optional[str] = ..., login_disabled: bool = ..., caller_ids: _Optional[_Iterable[str]] = ..., linkback_numbers: _Optional[_Iterable[str]] = ..., auth_user_id: _Optional[str] = ..., enable_mfa: bool = ..., first_name: _Optional[str] = ..., last_name: _Optional[str] = ..., created: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., last_updated: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., password_reset_required: bool = ..., connection_id: _Optional[_Union[_wrappers_pb2.StringValue, _Mapping]] = ..., time_zone_override: _Optional[_Union[_org_pb2.TimeZoneWrapper, _Mapping]] = ..., permission_group_ids: _Optional[_Iterable[str]] = ..., trust_ids: _Optional[_Iterable[str]] = ..., default_region: _Optional[str] = ..., default_application: _Optional[_Union[_org_pb2.OperatorApplications, str]] = ..., user_caller_id: _Optional[str] = ..., agent_profile_group_id: _Optional[str] = ..., skills: _Optional[_Iterable[_Union[Skill, _Mapping]]] = ..., agent: bool = ..., account_owner: bool = ..., email_verified: bool = ...) -> None: ...
+    whitelisted_ips: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, user_id: _Optional[str] = ..., org_id: _Optional[str] = ..., username: _Optional[str] = ..., p3_permission_group_id: _Optional[str] = ..., login_sid: _Optional[int] = ..., agent_sid: _Optional[int] = ..., region_id: _Optional[str] = ..., partner_agent_id: _Optional[str] = ..., client_sid: _Optional[int] = ..., region_sid_map: _Optional[_Mapping[str, User.RegionSids]] = ..., api_key: _Optional[str] = ..., email: _Optional[str] = ..., login_disabled: bool = ..., caller_ids: _Optional[_Iterable[str]] = ..., linkback_numbers: _Optional[_Iterable[str]] = ..., auth_user_id: _Optional[str] = ..., enable_mfa: bool = ..., first_name: _Optional[str] = ..., last_name: _Optional[str] = ..., created: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., last_updated: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., password_reset_required: bool = ..., connection_id: _Optional[_Union[_wrappers_pb2.StringValue, _Mapping]] = ..., time_zone_override: _Optional[_Union[_org_pb2.TimeZoneWrapper, _Mapping]] = ..., permission_group_ids: _Optional[_Iterable[str]] = ..., trust_ids: _Optional[_Iterable[str]] = ..., default_region: _Optional[str] = ..., default_application: _Optional[_Union[_org_pb2.OperatorApplications, str]] = ..., user_caller_id: _Optional[str] = ..., agent_profile_group_id: _Optional[str] = ..., skills: _Optional[_Iterable[_Union[Skill, _Mapping]]] = ..., agent: bool = ..., account_owner: bool = ..., email_verified: bool = ..., whitelisted_ips: _Optional[_Iterable[str]] = ...) -> None: ...
 
 class MfaInfo(_message.Message):
     __slots__ = ("org_id", "user_id", "mfa_enabled", "none", "otp", "duo")
