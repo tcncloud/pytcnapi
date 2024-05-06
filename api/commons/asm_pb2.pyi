32c3104b85e8c8a9375c5c64f3e5ec571b62bdb8
TCN CI <ci@tcnp3.com>
2024-05-06 19:59:33 +0000

    update pytcnapi > added uuid to manager stream agent state response

--- api/commons/asm_pb2.pyi
+++ api/commons/asm_pb2.pyi
@@ -108,12 +108,14 @@ class StreamAgentStateRes(_message.Message):
     def __init__(self, state: _Optional[_Union[_acd_pb2.AgentState, _Mapping]] = ..., heart_beat: _Optional[_Union[KeepAlive, _Mapping]] = ..., call_queue_add: _Optional[_Union[QueueCallAdd, _Mapping]] = ..., call_queue_remove: _Optional[_Union[QueueCallRemove, _Mapping]] = ...) -> None: ...
 
 class ManagerStreamAgentStateRes(_message.Message):
-    __slots__ = ("state", "heart_beat")
+    __slots__ = ("state", "heart_beat", "uuid")
     STATE_FIELD_NUMBER: _ClassVar[int]
     HEART_BEAT_FIELD_NUMBER: _ClassVar[int]
+    UUID_FIELD_NUMBER: _ClassVar[int]
     state: _acd_pb2.AgentState
     heart_beat: KeepAlive
-    def __init__(self, state: _Optional[_Union[_acd_pb2.AgentState, _Mapping]] = ..., heart_beat: _Optional[_Union[KeepAlive, _Mapping]] = ...) -> None: ...
+    uuid: str
+    def __init__(self, state: _Optional[_Union[_acd_pb2.AgentState, _Mapping]] = ..., heart_beat: _Optional[_Union[KeepAlive, _Mapping]] = ..., uuid: _Optional[str] = ...) -> None: ...
 
 class KeepAlive(_message.Message):
     __slots__ = ()
