177da1bc6a5a9b45334b35405d16b48c5758d050
TCN CI <ci@tcnp3.com>
2024-05-21 20:44:45 +0000

    update pytcnapi > Merge branch '537-add-option-for-classifier' into 'master'
    Resolve "Add option for Classifier"
    
    Closes #537
    
    See merge request tcnpublic/api!564

--- api/v1alpha1/classifier/service_pb2.pyi
+++ api/v1alpha1/classifier/service_pb2.pyi
@@ -1,4 +1,6 @@
+from annotations import authz_pb2 as _authz_pb2
 from api.v1alpha1.classifier import entities_pb2 as _entities_pb2
+from google.api import annotations_pb2 as _annotations_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
