eb89d9854a5b1282f0a09ea66859f5d9b13348ad
TCN CI <ci@tcnp3.com>
2024-12-17 21:21:32 +0000

    update pytcnapi > Merge branch '806-explorer-add-insight-pipeline-to-queryrequest' into 'master'
    Resolve "Explorer - Add insight pipeline to QueryRequest"
    
    Closes #806
    
    See merge request tcnpublic/api!932

--- api/v1alpha1/explorer/service_pb2.pyi
+++ api/v1alpha1/explorer/service_pb2.pyi
@@ -1,5 +1,6 @@
 from annotations import authz_pb2 as _authz_pb2
 from api.v1alpha1.explorer import entities_pb2 as _entities_pb2
+from api.v1alpha1.insights import insight_content_pb2 as _insight_content_pb2
 from google.api import annotations_pb2 as _annotations_pb2
 from google.protobuf import timestamp_pb2 as _timestamp_pb2
 from google.protobuf.internal import containers as _containers
@@ -38,11 +39,12 @@ class ListDatasourceSchemasResponse(_message.Message):
     def __init__(self, schemas: _Optional[_Iterable[_Union[_entities_pb2.Schema, _Mapping]]] = ...) -> None: ...
 
 class QueryRequest(_message.Message):
-    __slots__ = ("datasource_name", "datasource_type", "pipeline", "prql", "org_ids", "start_time", "end_time", "timezone", "pipeline_parameters", "ui_trace_id", "comment", "format")
+    __slots__ = ("datasource_name", "datasource_type", "pipeline", "prql", "insight_body", "org_ids", "start_time", "end_time", "timezone", "pipeline_parameters", "ui_trace_id", "comment", "format")
     DATASOURCE_NAME_FIELD_NUMBER: _ClassVar[int]
     DATASOURCE_TYPE_FIELD_NUMBER: _ClassVar[int]
     PIPELINE_FIELD_NUMBER: _ClassVar[int]
     PRQL_FIELD_NUMBER: _ClassVar[int]
+    INSIGHT_BODY_FIELD_NUMBER: _ClassVar[int]
     ORG_IDS_FIELD_NUMBER: _ClassVar[int]
     START_TIME_FIELD_NUMBER: _ClassVar[int]
     END_TIME_FIELD_NUMBER: _ClassVar[int]
@@ -55,6 +57,7 @@ class QueryRequest(_message.Message):
     datasource_type: _entities_pb2.DatasourceType
     pipeline: str
     prql: str
+    insight_body: _insight_content_pb2.Pipeline
     org_ids: _containers.RepeatedScalarFieldContainer[str]
     start_time: _timestamp_pb2.Timestamp
     end_time: _timestamp_pb2.Timestamp
@@ -63,7 +66,7 @@ class QueryRequest(_message.Message):
     ui_trace_id: str
     comment: str
     format: _entities_pb2.ExportFormat
-    def __init__(self, datasource_name: _Optional[str] = ..., datasource_type: _Optional[_Union[_entities_pb2.DatasourceType, str]] = ..., pipeline: _Optional[str] = ..., prql: _Optional[str] = ..., org_ids: _Optional[_Iterable[str]] = ..., start_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., end_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., timezone: _Optional[str] = ..., pipeline_parameters: _Optional[_Union[_entities_pb2.Parameters, _Mapping]] = ..., ui_trace_id: _Optional[str] = ..., comment: _Optional[str] = ..., format: _Optional[_Union[_entities_pb2.ExportFormat, str]] = ...) -> None: ...
+    def __init__(self, datasource_name: _Optional[str] = ..., datasource_type: _Optional[_Union[_entities_pb2.DatasourceType, str]] = ..., pipeline: _Optional[str] = ..., prql: _Optional[str] = ..., insight_body: _Optional[_Union[_insight_content_pb2.Pipeline, _Mapping]] = ..., org_ids: _Optional[_Iterable[str]] = ..., start_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., end_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., timezone: _Optional[str] = ..., pipeline_parameters: _Optional[_Union[_entities_pb2.Parameters, _Mapping]] = ..., ui_trace_id: _Optional[str] = ..., comment: _Optional[str] = ..., format: _Optional[_Union[_entities_pb2.ExportFormat, str]] = ...) -> None: ...
 
 class QueryResponse(_message.Message):
     __slots__ = ("result_url", "result_size_bytes")
