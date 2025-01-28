ad0ccc51d50d71114bcfd8fbb9e0ef65745b6bc1
TCN CI <ci@tcnp3.com>
2025-01-28 21:54:14 +0000

    update pytcnapi > Merge branch '844-add-new-fields-to-listreportlogs-public-api-endpoint' into 'master'
    Resolve "Add New Fields to ListReportLogs Public API Endpoint"
    
    Closes #844
    
    See merge request tcnpublic/api!979

--- api/v1alpha1/bireportgenerator/entities_pb2.pyi
+++ api/v1alpha1/bireportgenerator/entities_pb2.pyi
@@ -53,7 +53,7 @@ class ReportJob(_message.Message):
     def __init__(self, report_job_id: _Optional[str] = ..., name: _Optional[str] = ..., description: _Optional[str] = ..., dashboard_id: _Optional[str] = ..., time_zone: _Optional[str] = ..., time_period: _Optional[_Union[_bireportgenerator_pb2.TimePeriod, str]] = ..., delivery_times: _Optional[_Union[_bireportgenerator_pb2.DeliveryTimes, _Mapping]] = ..., day_filter: _Optional[_Union[_bireportgenerator_pb2.DayFilter, _Mapping]] = ..., months: _Optional[_Iterable[_Union[_enums_pb2.Month, str]]] = ..., format_options: _Optional[_Union[_bireportgenerator_pb2.FormatOptions, _Mapping]] = ..., delivery_options: _Optional[_Union[_bireportgenerator_pb2.DeliveryOptions, _Mapping]] = ..., is_active: bool = ..., send_empty_report: bool = ..., dashboard_resource_id: _Optional[str] = ..., time_zone_wrapper: _Optional[_Union[_org_pb2.TimeZoneWrapper, _Mapping]] = ..., hide_csv_footer: bool = ..., transfer_config_sid: _Optional[int] = ..., cron_expression: _Optional[_Union[_types_pb2.CronExpression, _Mapping]] = ..., transfer_options: _Optional[_Union[_bireportgenerator_pb2.TransferOptions, _Mapping]] = ...) -> None: ...
 
 class ReportLog(_message.Message):
-    __slots__ = ("org_id", "report_log_id", "report_job_id", "execution_id", "report_name", "job_requested_time", "job_completed_time", "success", "failure_reason", "attempt_number", "max_attempts", "create_time", "update_time")
+    __slots__ = ("org_id", "report_log_id", "report_job_id", "execution_id", "report_name", "job_requested_time", "job_completed_time", "success", "failure_reason", "attempt_number", "max_attempts", "create_time", "update_time", "dashboard_title", "scheduled_time", "execution_start_time", "execution_end_time", "report_start_date", "report_end_date", "comments", "timezone", "filenames", "insight_count", "delivery_definition_title")
     ORG_ID_FIELD_NUMBER: _ClassVar[int]
     REPORT_LOG_ID_FIELD_NUMBER: _ClassVar[int]
     REPORT_JOB_ID_FIELD_NUMBER: _ClassVar[int]
@@ -67,6 +67,17 @@ class ReportLog(_message.Message):
     MAX_ATTEMPTS_FIELD_NUMBER: _ClassVar[int]
     CREATE_TIME_FIELD_NUMBER: _ClassVar[int]
     UPDATE_TIME_FIELD_NUMBER: _ClassVar[int]
+    DASHBOARD_TITLE_FIELD_NUMBER: _ClassVar[int]
+    SCHEDULED_TIME_FIELD_NUMBER: _ClassVar[int]
+    EXECUTION_START_TIME_FIELD_NUMBER: _ClassVar[int]
+    EXECUTION_END_TIME_FIELD_NUMBER: _ClassVar[int]
+    REPORT_START_DATE_FIELD_NUMBER: _ClassVar[int]
+    REPORT_END_DATE_FIELD_NUMBER: _ClassVar[int]
+    COMMENTS_FIELD_NUMBER: _ClassVar[int]
+    TIMEZONE_FIELD_NUMBER: _ClassVar[int]
+    FILENAMES_FIELD_NUMBER: _ClassVar[int]
+    INSIGHT_COUNT_FIELD_NUMBER: _ClassVar[int]
+    DELIVERY_DEFINITION_TITLE_FIELD_NUMBER: _ClassVar[int]
     org_id: str
     report_log_id: int
     report_job_id: int
@@ -80,4 +91,15 @@ class ReportLog(_message.Message):
     max_attempts: int
     create_time: _timestamp_pb2.Timestamp
     update_time: _timestamp_pb2.Timestamp
-    def __init__(self, org_id: _Optional[str] = ..., report_log_id: _Optional[int] = ..., report_job_id: _Optional[int] = ..., execution_id: _Optional[str] = ..., report_name: _Optional[str] = ..., job_requested_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., job_completed_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., success: bool = ..., failure_reason: _Optional[str] = ..., attempt_number: _Optional[int] = ..., max_attempts: _Optional[int] = ..., create_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., update_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ...) -> None: ...
+    dashboard_title: str
+    scheduled_time: _timestamp_pb2.Timestamp
+    execution_start_time: _timestamp_pb2.Timestamp
+    execution_end_time: _timestamp_pb2.Timestamp
+    report_start_date: _timestamp_pb2.Timestamp
+    report_end_date: _timestamp_pb2.Timestamp
+    comments: _containers.RepeatedScalarFieldContainer[str]
+    timezone: str
+    filenames: _containers.RepeatedScalarFieldContainer[str]
+    insight_count: int
+    delivery_definition_title: str
+    def __init__(self, org_id: _Optional[str] = ..., report_log_id: _Optional[int] = ..., report_job_id: _Optional[int] = ..., execution_id: _Optional[str] = ..., report_name: _Optional[str] = ..., job_requested_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., job_completed_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., success: bool = ..., failure_reason: _Optional[str] = ..., attempt_number: _Optional[int] = ..., max_attempts: _Optional[int] = ..., create_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., update_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., dashboard_title: _Optional[str] = ..., scheduled_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., execution_start_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., execution_end_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., report_start_date: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., report_end_date: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., comments: _Optional[_Iterable[str]] = ..., timezone: _Optional[str] = ..., filenames: _Optional[_Iterable[str]] = ..., insight_count: _Optional[int] = ..., delivery_definition_title: _Optional[str] = ...) -> None: ...
