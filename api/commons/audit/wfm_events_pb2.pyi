12e622d4cac95332d9d80d42c00635c9e13dcba3
TCN CI <ci@tcnp3.com>
2025-03-04 21:33:43 +0000

    update pytcnapi > Merge branch '871-wfo-ca-define-billing-agent-seats-event' into 'master'
    Resolve "WFM define billing agent seats event"
    
    Closes wfo/wfo#79 and #871
    
    See merge request tcnpublic/api!1020

---
+++ api/commons/audit/wfm_events_pb2.pyi
@@ -0,0 +1,15 @@
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class WFMPublishScheduleEvent(_message.Message):
+    __slots__ = ("published_schedule_sid", "user_ids")
+    PUBLISHED_SCHEDULE_SID_FIELD_NUMBER: _ClassVar[int]
+    USER_IDS_FIELD_NUMBER: _ClassVar[int]
+    published_schedule_sid: int
+    user_ids: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, published_schedule_sid: _Optional[int] = ..., user_ids: _Optional[_Iterable[str]] = ...) -> None: ...
